830ca683f94cc9797061e8c33e41a88d1c546438
Dan <dmalengela@yahoo.com>
2023-05-04 12:06:39 +0200

    Uncommented the customaction1config.cs class

--- CustomAction/CustomAction1Config.cs
+++ CustomAction/CustomAction1Config.cs
@@ -5,11 +5,11 @@ namespace CustomAction
 {
     public class ChangeFormFieldValueConfig : PluginConfiguration
     {
-        //[ConfigEditableText(DisplayName = "Source form field ID")]
-        //public string SourceFormFieldID { get; set; }
+        [ConfigEditableText(DisplayName = "Source form field ID")]
+        public string SourceFormFieldID { get; set; }
 
-        //[ConfigEditableText(DisplayName = "Destination form field ID")]
-        //public string DestinationFormFieldID { get; set; }
+        [ConfigEditableText(DisplayName = "Destination form field ID")]
+        public string DestinationFormFieldID { get; set; }
 
         [ConfigEditableText(DisplayName = "Price form field ID", Description = "ID of the form field which contains price of the product")]
         public string PriceFormFieldID { get; set; }
